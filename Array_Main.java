113c28d0f384fc491c241a274649a26f664bf86e
박성진 <sjpark3579@gmail.com>
2019-05-08 19:03:04 +0900

    hello

---
+++ Array_Main.java
@@ -0,0 +1,19 @@
+package arrayList;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Array_Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("성적 관리 프로그램");
+		System.out.println("번호를 눌러 수행할 작업을 선택하세요.");
+		System.out.println("1. ");
+		
+		
+
+	}
+
+}
